b2fa55d27bab71854813a10b65739eec8f5486db
Alex  Pereira de Oliveira <alex.oliveira@platos.parceirosedu.com.br>
2024-01-24 06:49:28 -0300

    fix pupetter

--- helper/index.js
+++ helper/index.js
@@ -1,6 +1,6 @@
 const csvtojson = require('csvtojson');
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 const fs = require('fs');
